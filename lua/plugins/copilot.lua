223f6130f9b8e1d66ea9f9e373e6793888f6ac5f
modolet <modolet@163.com>
2024-02-23 15:38:55 +0800

    AI补全

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+      },
+      filetypes = {
+        slint = false,
+      },
+      -- suggestion = { enabled = false },
+      -- panel = { enabled = false },
+    },
+  },
+}
