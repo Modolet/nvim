36fa0ec5c0344824c2e60379525faa63f28dd6f6
modolet <modolet@163.com>
2024-02-23 15:38:25 +0800

    设置neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.tabstop = 4
-vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+
+local opt = vim.opt
+
+opt.tabstop = 4
+opt.expandtab = true
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.swapfile = false
+
+if vim.g.neovide then
+  vim.o.guifont = "CaskaydiaCove Nerd Font Mono:h14" -- text below applies for VimScript
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+end
