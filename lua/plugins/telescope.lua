d3fdf7fa67550c72f43c0dbe7336b7ae37a6ebf9
modolet <modolet@163.com>
2024-02-23 15:40:28 +0800

    设置ctrl+j/k上下移动的keymap

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        mappings = {
+          i = {
+            ["<c-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<c-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+}
