95d160c2e83dd66b39de4edd68f13de7fb838c67
modolet <modolet@163.com>
2024-02-23 17:09:14 +0800

    添加了终端相关配置

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,93 @@
+local function toggle_lazygit()
+  _G.lazygit:toggle()
+end
+
+local function toggle_ranger()
+  _G.ranger:toggle()
+end
+
+local function toggle_horizonal_terminal()
+  _G.horizonal_terminal:toggle()
+end
+
+local function toggle_vertical_terminal()
+  _G.vertical_terminal:toggle()
+end
+
+local function toggle_float_terminal()
+  _G.float_terminal:toggle()
+end
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    keys = {
+      {
+        "<leader>tt",
+        toggle_float_terminal,
+        mode = { "n", "v" },
+        desc = "Float terminal",
+      },
+      {
+        "<c-l>",
+        toggle_float_terminal,
+        mode = { "n", "t", "i", "v" },
+        desc = "Float terminal",
+      },
+      {
+        "<leader>tg",
+        toggle_lazygit,
+        desc = "LazyGit",
+      },
+      {
+        "<leader>tr",
+        toggle_ranger,
+        desc = "Ranger",
+      },
+      {
+        "<leader>th",
+        toggle_horizonal_terminal,
+        desc = "horziontal terminal",
+      },
+      {
+        "<leader>tv",
+        toggle_vertical_terminal,
+        desc = "vertical terminal",
+      },
+    },
+    config = function()
+      require("toggleterm").setup()
+      _G.lazygit = require("toggleterm.terminal").Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+      })
+      _G.ranger = require("toggleterm.terminal").Terminal:new({
+        cmd = "ranger",
+        hidden = true,
+        direction = "float",
+      })
+      _G.horizonal_terminal = require("toggleterm.terminal").Terminal:new({
+        hidden = true,
+        direction = "horizontal",
+      })
+      _G.vertical_terminal = require("toggleterm.terminal").Terminal:new({
+        hidden = true,
+        direction = "vertical",
+      })
+      _G.float_terminal = require("toggleterm.terminal").Terminal:new({
+        hidden = true,
+        direction = "float",
+      })
+    end,
+  },
+  {
+    "willothy/flatten.nvim",
+    opts = {
+      window = {
+        open = "alternate",
+      },
+    },
+    lazy = false,
+    priority = 1001,
+  },
+}
