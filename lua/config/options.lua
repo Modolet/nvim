2035514f7659ab13988d1de65341da305d597078
modolet <modolet@163.com>
2024-03-18 23:03:03 +0800

    傻逼windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 local opt = vim.opt
 
 opt.tabstop = 4
@@ -19,4 +15,8 @@ if vim.g.neovide then
   vim.g.neovide_light_angle_degrees = 45
   vim.g.neovide_light_radius = 5
   vim.g.neovide_cursor_vfx_mode = "railgun"
+  if vim.fn.has("win32") then
+    -- vim.g.neovide_transparency = 0.8
+    -- vim.g.neovide_window_blurred = true
+  end
 end
