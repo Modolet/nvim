a75008a3310c998e8c8987095c96bd1c0e54391b
modolet <modolet@163.com>
2024-03-18 22:28:17 +0800

    删除默认终端，添加行首行尾映射

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<ESC>", { silent = true, noremap = true })
+vim.keymap.set("n", "H", "^", { noremap = true })
+vim.keymap.set("n", "L", "$", { noremap = true })
+vim.keymap.del("n", "<c-/>")
+vim.keymap.del("t", "<c-/>")
