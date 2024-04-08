c8aab79b852a0a12df13901936073e1ebbd876ab
modolet <modolet@163.com>
2024-04-09 02:50:19 +0800

    fixme:slint文件不生效

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,6 +1,20 @@
+local ft = { "html", "css", "scss", "vue", "slint", "toml", "json" }
 return {
   {
     "norcalli/nvim-colorizer.lua",
-    config = function() end,
+    ft = ft,
+    config = function()
+      require("colorizer").setup(ft, {
+        RGB = true,
+        RRGGBB = true,
+        names = true,
+        RRGGBBAA = true,
+        rgb_fn = true,
+        hsl_fn = true,
+        css = true,
+        css_fn = true,
+        mode = "background",
+      })
+    end,
   },
 }
