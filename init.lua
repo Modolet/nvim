2dd75224984caafa6a08286c1a1a6b530d721d89
modolet <modolet@163.com>
2024-04-12 23:37:32 +0800

    使用vscode时不自动变更主题

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-if vim.fn.has("unix") == 1 and vim.fn.executable("gsettings") == 1 then
+if vim.fn.has("unix") == 1 and vim.fn.executable("gsettings") == 1 and not vim.g.vscode then
   require("script.auto_colorscheme").set_color_scheme_from_system()
 end
